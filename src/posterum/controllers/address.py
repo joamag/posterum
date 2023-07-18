91888c377854a1c44d92cb0b2354b82937a62d2b
João Magalhães <joamag@gmail.com>
2023-07-19 00:06:26 +0100

    chore: initial validation

--- src/posterum/controllers/address.py
+++ src/posterum/controllers/address.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import aiodns
 import appier
+import aiosmtplib
+
+from json import dumps
 
 from .root import RootController
 
@@ -9,7 +13,67 @@ from .root import RootController
 class AddressController(RootController):
     @appier.route("/v1/addresses/validate", "GET", json=True)
     async def validate(self):
-        address = self.field("address", mandatory=True)
+        address = self.field("email")
+        address = self.field("address", address)
         key = self.field("key", mandatory=True)
 
-        return dict(address=address, key=key)
+        if not key == "123":
+            raise appier.SecurityError(message="Invalid key")
+
+        if not address:
+            raise appier.OperationalError(message="Missing email address")
+
+        result = await is_valid_email(address)
+
+        print(result)
+
+        return dumps(dict(address=address, key=key, result=result))
+
+
+async def get_mx_records(domain):
+    resolver = aiodns.DNSResolver()
+    try:
+        result = await resolver.query(domain, "MX")
+        return [str(mx.host) for mx in result]
+    except aiodns.error.DNSError:
+        return []
+
+
+async def is_valid_email(email) -> bool:
+    domain = email.split("@")[1]
+    mx_servers = await get_mx_records(domain)
+
+    print(mx_servers)
+
+    for mx_server in mx_servers:
+        smtp_client = aiosmtplib.SMTP(hostname=mx_server)
+        try:
+            await smtp_client.connect()
+            response = await smtp_client.ehlo()
+
+            try:
+                response = await smtp_client.vrfy(email)
+                if response[0] == 250:
+                    return True
+            except aiosmtplib.SMTPResponseException:
+                pass
+
+            await smtp_client.mail("noreply@bemisc.com")
+
+            try:
+                response = await smtp_client.rcpt(email)
+                if response[0] == 250:
+                    return True
+            except aiosmtplib.SMTPResponseException as exception:
+                print(exception)
+                pass
+
+            return False
+        except Exception as exception:
+            print("Exception: ", exception.__class__)
+            print(exception)
+            return False
+        finally:
+            await smtp_client.quit()
+
+    return False
