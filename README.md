bf859bc5fb68b249221cc5eaaad461bf5c385ee8
João Magalhães <joamag@gmail.com>
2023-07-19 10:38:09 +0100

    chore: new self refactor

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Simple e-mail address verification service.
 * [Catch-all](https://en.wikipedia.org/wiki/Email_filtering#Methods) tentative verification
 * Email service differentiation (ex: Microsoft, Google, Yahoo, Zoho, etc.)
 * Horizontal scalability
+
+## Installation
+
+## Development
+
+1. Make sure you are running in a virtual environment (e.g., `python3 -m venv .venv`)
+2. Activate it (e.g. `source .venv/bin/activate` or `.\.venv\Scripts\Activate.ps1`)
+3. Install dependencies (we use [pip-tools](https://github.com/jazzband/pip-tools) for dependency management)
--- src/posterum/controllers/address.py
+++ src/posterum/controllers/address.py
@@ -23,57 +23,58 @@ class AddressController(RootController):
         if not address:
             raise appier.OperationalError(message="Missing email address")
 
-        result = await is_valid_email(address)
+        # runs the effective SMTP validation test for the address
+        # and obtains the result of the validation, to be used in
+        # the sending of the response
+        result = await self.is_valid_email(address)
 
-        print(result)
-
-        return dumps(dict(address=address, key=key, result=result))
-
-
-async def get_mx_records(domain):
-    resolver = aiodns.DNSResolver()
-    try:
-        result = await resolver.query(domain, "MX")
-        return [str(mx.host) for mx in result]
-    except aiodns.error.DNSError:
-        return []
+        return dumps(dict(address=address, result=result))
 
 
-async def is_valid_email(email) -> bool:
-    domain = email.split("@")[1]
-    mx_servers = await get_mx_records(domain)
-
-    print(mx_servers)
-
-    for mx_server in mx_servers:
-        smtp_client = aiosmtplib.SMTP(hostname=mx_server)
+    async def get_mx_records(domain):
+        resolver = aiodns.DNSResolver()
         try:
-            await smtp_client.connect()
-            response = await smtp_client.ehlo()
+            result = await resolver.query(domain, "MX")
+            return [str(mx.host) for mx in result]
+        except aiodns.error.DNSError:
+            return []
 
+
+    async def is_valid_email(email) -> bool:
+        domain = email.split("@")[1]
+        mx_servers = await self.get_mx_records(domain)
+
+        print(mx_servers)
+
+        for mx_server in mx_servers:
+            smtp_client = aiosmtplib.SMTP(hostname=mx_server)
             try:
-                response = await smtp_client.vrfy(email)
-                if response[0] == 250:
-                    return True
-            except aiosmtplib.SMTPResponseException:
-                pass
+                await smtp_client.connect()
+                response = await smtp_client.ehlo()
 
-            await smtp_client.mail("noreply@bemisc.com")
+                try:
+                    response = await smtp_client.vrfy(email)
+                    if response[0] == 250:
+                        return True
+                except aiosmtplib.SMTPResponseException:
+                    pass
 
-            try:
-                response = await smtp_client.rcpt(email)
-                if response[0] == 250:
-                    return True
-            except aiosmtplib.SMTPResponseException as exception:
+                await smtp_client.mail("noreply@bemisc.com")
+
+                try:
+                    response = await smtp_client.rcpt(email)
+                    if response[0] == 250:
+                        return True
+                except aiosmtplib.SMTPResponseException as exception:
+                    print(exception)
+                    pass
+
+                return False
+            except Exception as exception:
+                print("Exception: ", exception.__class__)
                 print(exception)
-                pass
+                return False
+            finally:
+                await smtp_client.quit()
 
-            return False
-        except Exception as exception:
-            print("Exception: ", exception.__class__)
-            print(exception)
-            return False
-        finally:
-            await smtp_client.quit()
-
-    return False
+        return False
